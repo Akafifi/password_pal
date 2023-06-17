a108ed46e12d49f639fb27815fa33b7fc040f3da
Akafifi <itsmeafifi@gmail.com>
2023-06-16 21:11:17 -0500

    added length if then

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,18 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(event) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password
+}
 
+function generatePassword(){
+  var length = prompt("Enter a password length: " )
+  if(length < 8 || length > 128){
+    alert("ERROR!")
+  }
 }
 
 // Add event listener to generate button
