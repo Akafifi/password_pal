67e477d487628b36ba8a7edf40bcdad5d0f7c97b
Akafifi <itsmeafifi@gmail.com>
2023-06-17 15:13:54 -0500

    added for loop

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword(event) {
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -10,8 +10,8 @@ function writePassword(event) {
 }
 
 function generatePassword(){
-  var length = prompt("Enter a password length: " )
-  if(length < 8 || length > 128){
+  var passLength = prompt("Enter a password length: " )
+  if(passLength < 8 || passLength > 128){
     alert("ERROR!")
   }
   var lowerCase = prompt("Lowercase? Y/N")
@@ -20,11 +20,13 @@ function generatePassword(){
     var upperCase = prompt("Uppercase? Y/N")
   }
 
-  
+  var specialChar = prompt("Special characters? Y/N?")
 
-  
-
-
+  var specialString = ""
+  for(var i=0; i < passLength; i++){
+    specialString = specialString + String.fromCharCode(Math.random(33,47))
+  }
+  return specialString
 }
 
 // Add event listener to generate button
