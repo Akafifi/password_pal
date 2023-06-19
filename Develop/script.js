0e37c8175fe61595d1eff47d5f8ae961f4f6ba72
Akafifi <itsmeafifi@gmail.com>
2023-06-18 23:29:56 -0500

    added range functions

--- Develop/script.js
+++ Develop/script.js
@@ -16,18 +16,71 @@ function generatePassword(){
   }
   var lowerCase = prompt("Lowercase? Y/N")
   
-  if (lowerCase.toLowerCase() == 'y'){
-    var upperCase = prompt("Uppercase? Y/N")
-  }
+  var upperCase = prompt("Uppercase? Y/N")
+
 
   var specialChar = prompt("Special characters? Y/N?")
 
   var specialString = ""
+  // Math.random(lowRange, highRange)
+  if (specialChar === 'Y') {
+    var lowRange = 33
+  } else {
+    var lowRange = 48
+  }
+
+  var highRange = 0
+  if (upperCase.toUpperCase() === 'Y' && lowerCase.toUpperCase () === 'Y') {
+    highRange = 122
+  } 
+  else if (upperCase.toUpperCase() === 'Y' && lowerCase.toUpperCase() === 'N'){
+    highRange = 90
+  }
+  else if(upperCase.toUpperCase() === 'N' && lowerCase.toUpperCase () === 'N'){
+    highRange = 57
+  }
+  else{
+    highRange == 122
+  }
+  
   for(var i=0; i < passLength; i++){
-    specialString = specialString + String.fromCharCode(Math.random(33,47))
+    if(upperCase.toUpperCase() === 'N' && lowerCase.toUpperCase() === 'Y'){
+      var randomNum = getRandomNumber(lowRange, 64, 91, 122)
+    }
+    else{
+      var randomNum = Math.random() * (highRange - lowRange) + lowRange
+    }
+    specialString = specialString + String.fromCharCode(randomNum)
   }
   return specialString
 }
 
+function getRandomNumber(min1, max1, min2, max2) {
+  var random = Math.floor(Math.random() * 2);
+  if (random === 0) {
+    return Math.floor(Math.random() * (max1 - min1 + 1) + min1);
+  } else {
+    return Math.floor(Math.random() * (max2 - min2 + 1) + min2);
+  }
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+////////////////////////// TEST AREA //////////////////////////////////
+// const myName = 'adam'
+// const myAge = '42'
+
+// // console.log(`Hello it's me ${myName} and my age is ${myAge}`);
+
+
+// var myBday = "December"
+// var myFood = "cake"
+
+// // console.log(`Hi my favorite food is ${myFood} and my birthday is in ${myBday}`)
+
+// var favPoke = "Typhlosion"
+// var favType = 'Dark'
+
+// console.log(`My favorite pokemon is ${favPoke} and my favorite type is ${favType}`)
